effcb9a581717a191d9a01825f6d2a437b1bb81b
Michaell Jimmy Mahan Rodriguez <michaell.mahan@neoris.com>
2020-03-05 12:23:18 +0100

    añadir importedescuento

--- app/CabeceraFactura.php
+++ app/CabeceraFactura.php
@@ -8,5 +8,5 @@ class CabeceraFactura extends Model
 {
     public $timestamps = false;
     protected $table = 'portal.cabecera_factura';
-    protected $fillable = ['id', 'id_user', 'id_cliente', 'seriefactura', 'numerofactura', 'fecha_factura', 'importebruto', 'pordescuento', 'baseimponible', 'totaliva', 'totalfactura', 'observacionesfactura'];
+    protected $fillable = ['id', 'id_user', 'id_cliente', 'seriefactura', 'numerofactura', 'fecha_factura', 'importebruto', 'pordescuento','importedescuento', 'baseimponible', 'totaliva', 'totalfactura', 'observacionesfactura'];
 }
